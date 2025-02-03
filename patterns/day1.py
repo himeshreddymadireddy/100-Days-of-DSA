81e8b13a62bddeee177351f1547e51cc623b9ced
himesh reddy madireddy <himeshmadireddy@gmail.com>
2025-02-03 21:04:38 +0530

    new patterns

--- patterns/day1.py
+++ patterns/day1.py
@@ -1,9 +1,5 @@
-#finding 2duplicates
-def find_duplicates(l):
-        n=set(l)
-        if len(l)==len(n):
-                return 0
-        else:
-                return 1
-     
-find_duplicates(l=[1,2,3,3])
+for i in range(5):
+        for j in range(5):
+            print("*",end="")
+        print()
+           
