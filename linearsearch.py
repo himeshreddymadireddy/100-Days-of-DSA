684195f7a389f5e5ad6939bcf77e15c95c22b483
himesh reddy madireddy <himeshmadireddy@gmail.com>
2025-02-17 02:06:32 +0530

    "modified linear search"

--- linearsearch.py
+++ linearsearch.py
@@ -2,10 +2,10 @@ def linearSearch(arr,idx):
     for i in range(len(arr)):
         if arr[i] == idx:
             return i
-        else:
-            return -1
+    else:
+        return -1
 
-arr=[1,4,70,90,110,20,30]
+arr=[1,4,20,30,70,90,110]
 idx=70
 
 result=linearSearch(arr,idx)
